901f524a002893a215181c1be65e9f001f3d63c7
ShayneDanos <111548875+ShayneDanos@users.noreply.github.com>
2026-03-02 01:24:37 +0800

    sa

--- Connect-U/src/main.tsx
+++ Connect-U/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import Index from './assets/Pages/Index'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Index/>
   </StrictMode>,
 )
